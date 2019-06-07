50a9eac75eb093e99c5ff7c57c6fbdd524b03d48
mnash29 <mtntrades10@gmail.com>
2019-06-07 19:09:16 +0000

    Add two numbers python soln

---
+++ Solutions/add_two_numbers.py
@@ -0,0 +1,32 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+#
+# Accepted
+# Runtime: 88ms, faster than 49.04% of python3 submissions
+# Memory: 13.3MB, less than 33.14% of python3 submissions
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        soln = ListNode(0)
+        curr = soln
+        sum = 0
+        while l1 or l2:
+            sum //= 10
+            if l1 and l2:
+                sum += (l1.val + l2.val)
+                l1 = l1.next
+                l2 = l2.next
+            elif l1:
+                sum += l1.val
+                l1 = l1.next
+            elif l2:
+                sum += l2.val
+                l2 = l2.next
+            curr.next = ListNode(sum % 10)
+            curr = curr.next
+        if sum // 10 == 1:
+            curr.next = ListNode(1)
+        return soln.next
